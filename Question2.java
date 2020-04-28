a7dcae5bcc7abd4249ccecd1d6ec7340a97d623d
amanda-chan <60087811+amanda-chan@users.noreply.github.com>
2020-04-28 22:35:52 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -9,8 +9,17 @@ public class Question2
      * 
      * Hint: in.nextDouble() to read double
      */
-     
+    
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter height (m): ");
     double height = in.nextDouble();
+    System.out.println("Height: " + height + " m");
+    Scanner w = new Scanner(System.in);
+    System.out.print("Enter weight (kg): ");
+    double weight = w.nextDouble();
+    System.out.println("Weight: " + weight + " kg");
+    double bmi = (weight / Math.pow(height, 2));
+    double Finalbmi = Math.round(bmi*100.0)/100.0;
+    System.out.println("Your BMI is: " + Finalbmi);
   }
 }
